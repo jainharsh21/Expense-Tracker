a5aca3bc21c454937d2e26cf1d886a5336dce2a1
HARSH JAIN <jainharsh270@gmail.com>
2020-06-20 11:50:38 +0530

    added balance,income expenses and transaction list component

--- src/App.js
+++ src/App.js
@@ -1,8 +1,21 @@
 import React from "react";
+import { Header } from "./components/Header";
 import "./App.css";
+import { Balance } from "./components/Balance";
+import { IncomeExpenses } from "./components/IncomeExpenses";
+import { TransactionList } from "./components/TransactionList";
 
 function App() {
-  return <div>Hello World!</div>;
+  return (
+    <div>
+      <Header />
+      <div className="container">
+        <Balance />
+        <IncomeExpenses />
+        <TransactionList />
+      </div>
+    </div>
+  );
 }
 
 export default App;
