5ef3d8231207e547a9142713110128f4683d05f4
HARSH JAIN <jainharsh270@gmail.com>
2020-06-22 12:20:38 +0530

    added add transaction and get transaction functionality

--- controllers/transactions.controller.js
+++ controllers/transactions.controller.js
@@ -1,11 +1,45 @@
-exports.getTransactions = (req, res, next) => {
-  res.send("GET transactions");
+const Transaction = require("../models/Transaction");
+
+exports.getTransactions = async (req, res, next) => {
+  try {
+    const transactions = await Transaction.find();
+    return res.status(200).json({
+      success: true,
+      count: transactions.length,
+      data: transactions,
+    });
+  } catch (error) {
+    return res.status(500).json({
+      success: false,
+      error: "Server Error",
+    });
+  }
 };
 
-exports.addTransaction = (req, res, next) => {
-  res.send("POST transaction");
+exports.addTransaction = async (req, res, next) => {
+  try {
+    const { text, amount } = req.body;
+
+    const transaction = await Transaction.create(req.body);
+
+    return res.status(201).json({ success: true, data: transaction });
+  } catch (err) {
+    if (err.name === "ValidationError") {
+      const messages = Object.values(err.errors).map((val) => val.message);
+
+      res.status(400).json({
+        success: false,
+        error: messages,
+      });
+    } else {
+      return res.status(500).json({
+        success: false,
+        error: "Server Error",
+      });
+    }
+  }
 };
 
-exports.deleteTransaction = (req, res, next) => {
+exports.deleteTransaction = async (req, res, next) => {
   res.send("DELETE transaction");
 };
