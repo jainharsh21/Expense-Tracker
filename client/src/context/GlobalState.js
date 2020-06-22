8461ae9e8b8c7f89b9a6f771df4c66624fc0ce8a
HARSH JAIN <jainharsh270@gmail.com>
2020-06-22 17:58:32 +0530

    connected cleint and server...getting all transactions

--- client/src/context/GlobalState.js
+++ client/src/context/GlobalState.js
@@ -1,8 +1,11 @@
 import React, { createContext, useReducer } from "react";
 import AppReducer from "./AppReducer";
+import axios from "axios";
 
 const initialState = {
   transactions: [],
+  error: null,
+  loading: true,
 };
 
 // Creating a context with initial state
@@ -14,6 +17,22 @@ export const GlobalProvider = ({ children }) => {
   const [state, dispatch] = useReducer(AppReducer, initialState);
 
   // Actions
+
+  async function getTransactions() {
+    try {
+      const res = await axios.get("/api/v1/transactions");
+      dispatch({
+        type: "GET_TRANSACTIONS",
+        payload: res.data.data,
+      });
+    } catch (err) {
+      dispatch({
+        type: "TRANSACTION_ERROR",
+        payload: err.response.data.error,
+      });
+    }
+  }
+
   function deleteTransaction(id) {
     dispatch({
       type: "DELETE_TRANSACTION",
@@ -32,7 +51,10 @@ export const GlobalProvider = ({ children }) => {
     <GlobalContext.Provider
       value={{
         transactions: state.transactions,
+        error: state.error,
+        loading : state.loading,
         deleteTransaction,
+        getTransactions,
         addTransaction,
       }}
     >
